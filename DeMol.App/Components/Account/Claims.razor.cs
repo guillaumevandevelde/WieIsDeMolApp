882bd1d9744a6d84301da655a9793d784cbea6ad
Van de Velde Guillaume <guillaume.vandevelde@district09.gent>
2024-03-30 20:06:00 +0100

    add roles

---
+++ DeMol.App/Components/Account/Claims.razor.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace DeMol.App.Components.Account;
+
+public partial class Claims
+{
+    [Inject]
+    public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
+    public IEnumerable<Claim> claims;
+    public ClaimsPrincipal user = default!;
+    
+    protected override async Task OnInitializedAsync()
+    {
+        var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        user = authState.User;
+        claims = user.Claims;
+    }
+}
--- DeMol.App/Program.cs
+++ DeMol.App/Program.cs
@@ -1,9 +1,14 @@
+using System.Security.Claims;
 using Auth0.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
 using DeMol.App.Components;
 using DeMol.App.Data;
 using DeMol.App.Services;
 using DeMol.App.Support;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 using Plk.Blazor.DragDrop;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,12 +19,79 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddAuth0WebAppAuthentication(options =>
-{
-    options.Domain = builder.Configuration["Auth0:Domain"];
-    options.ClientId = builder.Configuration["Auth0:ClientId"];
-    options.Scope = "openid email profile";
-});
+
+
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    })
+    .AddCookie()
+    .AddOpenIdConnect("Auth0", options =>
+    {
+        // Configureer deze opties met je Auth0 instellingen
+        options.Authority = builder.Configuration["Auth0:Domain"];
+        options.ClientId = builder.Configuration["Auth0:ClientId"];
+        options.ClientSecret = builder.Configuration["Auth0:ClientSecret"];
+        options.ResponseType = OpenIdConnectResponseType.Code;
+       
+
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("email");
+
+        options.CallbackPath = new PathString("/callback");
+        
+        
+        options.ClaimsIssuer = "Auth0";
+    
+        options.SaveTokens = true;
+        
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = "name",
+            RoleClaimType = "https://mijnapp.com/roles"
+        };
+        
+        options.Events = new OpenIdConnectEvents
+        {
+            OnRedirectToIdentityProviderForSignOut = (context) =>
+            {
+                var logoutUri =
+                    $"{builder.Configuration["Auth0:Domain"]}/v2/logout?client_id={builder.Configuration["Auth0:ClientId"]}";
+
+                var postLogoutUri = context.Properties.RedirectUri;
+                if (!string.IsNullOrEmpty(postLogoutUri))
+                {
+                    if (postLogoutUri.StartsWith("/"))
+                    {
+                        var request = context.Request;
+                        postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                    }
+
+                    logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
+                }
+
+                context.Response.Redirect(logoutUri);
+                context.HandleResponse();
+
+                return Task.CompletedTask;
+            },
+            OnTokenValidated = async ctx =>
+            {
+                var claimsIdentity = ctx.Principal.Identity as ClaimsIdentity;
+            }
+        };
+        
+        options.SaveTokens = true;
+    });
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("IsAdmin", policy =>
+        policy.RequireClaim("https://mijnapp.com/roles", "Admin"));
 
 builder.Services.ConfigureSameSiteNoneCookies();
 
